235d03d0745f5f2640ba653dc154259e19d1df93
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-08 11:43:56 -0300

    create migration users

---
+++ src/database/migrations/1607438097852-createUsers.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUsers1607438097852 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+          isNullable: false
+        },
+        {
+          name: 'email',
+          type: 'varchar',
+          isNullable: false,
+          isUnique: true
+        },
+        {
+          name: 'password',
+          type: 'varchar',
+          isNullable: false
+        },
+        {
+          name: 'createdAt',
+          type: 'timestamp',
+          default: 'now()',
+          isNullable: false
+        },
+        {
+          name: 'updatedAt',
+          type: 'timestamp',
+          default: 'now()',
+          isNullable: false
+        }
+      ]
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"')
+  }
+}
