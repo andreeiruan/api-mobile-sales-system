a69c5517109f34faa06c3ebba17c6eb085fdc4f6
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-08 22:16:20 -0300

    ajusts configures

--- babel.config.js
+++ babel.config.js
@@ -11,18 +11,23 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
     ['module-resolver', {
       alias: {
-        "@entities/*": "./src/entities/*",
-        "@config/*": "./src/config/*",
-        "@helpers/*": "./src/helpers/*",
-        "@providers/*": "./src/providers/*",
-        "@repositories/*": "./src/repositories/*",
-        "@useCases/*": "./src/useCases/*",
+        "@entities": "./src/app/entities",
+        "@config": "./src/config",
+        "@helpers": "./src/app/helpers",
+        "@providers": "./src/app/providers",
+        "@repositories": "./src/app/repositories",
+        "@useCases": "./src/app/useCases",
+        "@errors": "./src/app/helpers/errors",
+        "@app": "./src/app",
       }
     }]
   ],
   ignore: [
-    '**/*.spec.ts'
+    '**/*.spec.ts',
+    'src/database/migrations/*.ts'
   ]
 }
