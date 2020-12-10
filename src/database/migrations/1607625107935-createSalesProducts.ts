b8650195213bd436e0bc71d594251dd7c5675abc
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-10 15:38:51 -0300

    create migration of sales and salesProducts

---
+++ src/database/migrations/1607625107935-createSalesProducts.ts
@@ -0,0 +1,88 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class createSalesProducts1607625107935 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'salesProducts',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'unitaryValue',
+          type: 'float',
+          isNullable: false
+        },
+        {
+          name: 'discountUnitary',
+          type: 'float',
+          isNullable: false
+        },
+        {
+          name: 'amount',
+          type: 'int',
+          isNullable: false
+        },
+        {
+          name: 'userId',
+          type: 'uuid',
+          isNullable: false
+        },
+        {
+          name: 'productId',
+          type: 'uuid',
+          isNullable: false
+        },
+        {
+          name: 'saleId',
+          type: 'uuid',
+          isNullable: false
+        },
+        {
+          name: 'createdAt',
+          type: 'timestamp',
+          default: 'now()',
+          isNullable: false
+        },
+        {
+          name: 'updatedAt',
+          type: 'timestamp',
+          default: 'now()',
+          isNullable: false
+        }
+      ]
+    }))
+
+    await queryRunner.createForeignKey('salesProducts', new TableForeignKey({
+      columnNames: ['userId'],
+      referencedTableName: 'users',
+      referencedColumnNames: ['id'],
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    }))
+
+    await queryRunner.createForeignKey('salesProducts', new TableForeignKey({
+      columnNames: ['productId'],
+      referencedTableName: 'products',
+      referencedColumnNames: ['id'],
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    }))
+
+    await queryRunner.createForeignKey('salesProducts', new TableForeignKey({
+      columnNames: ['saleId'],
+      referencedTableName: 'sales',
+      referencedColumnNames: ['id'],
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('salesProducts')
+  }
+}
