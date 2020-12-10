e90ee5f4002c1aa976e20449a4bc69240705ef17
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-10 08:31:08 -0300

    Separate the log files by their level

--- src/app/helpers/Logger.ts
+++ src/app/helpers/Logger.ts
@@ -2,9 +2,10 @@ import { AwsS3 } from '@providers/AwsS3'
 import winston from 'winston'
 
 class AppLogger {
-  private readonly _optionsInfo = {
+  private readonly _awsS3: AwsS3
+  private logger: winston.Logger
+  private readonly _optionsLog = {
     file: {
-      filename: `${__dirname}/../../temp/logs/app.log`,
       handleExceptions: true,
       json: true,
       maxsize: 5242880,
@@ -13,37 +14,48 @@ class AppLogger {
     }
   }
 
-  private readonly _awsS3: AwsS3
-
-  private readonly logger: winston.Logger
   constructor (awsS3: AwsS3) {
     this._awsS3 = awsS3
-    this.logger = winston.createLogger({
-      defaultMeta: { date: new Date().toLocaleString() },
-      transports: [
-        new winston.transports.File(this._optionsInfo.file)
-      ]
-    })
   }
 
   logInfo (data: any) {
+    this.logger = winston.createLogger({
+      defaultMeta: { date: new Date().toLocaleString() },
+      transports: [
+        new winston.transports.File({ filename: `${__dirname}/../../temp/logs/info.log`, ...this._optionsLog.file })
+      ]
+    })
+
     this.logger.info(data)
     if (process.env.NODE_ENV === 'production') {
-      this._awsS3.uploadObj(this._optionsInfo.file.filename, process.env.BUCKET_LOGS)
+      this._awsS3.uploadObj(`${__dirname}/../../temp/logs/info.log`, process.env.BUCKET_LOGS)
     }
   }
 
   logError (data: any) {
+    this.logger = winston.createLogger({
+      defaultMeta: { date: new Date().toLocaleString() },
+      transports: [
+        new winston.transports.File({ filename: `${__dirname}/../../temp/logs/error.log`, ...this._optionsLog.file })
+      ]
+    })
+
     this.logger.error(data)
     if (process.env.NODE_ENV === 'production') {
-      this._awsS3.uploadObj(this._optionsInfo.file.filename, process.env.BUCKET_LOGS)
+      this._awsS3.uploadObj(`${__dirname}/../../temp/logs/error.log`, process.env.BUCKET_LOGS)
     }
   }
 
   logWarn (data: any) {
+    this.logger = winston.createLogger({
+      defaultMeta: { date: new Date().toLocaleString() },
+      transports: [
+        new winston.transports.File({ filename: `${__dirname}/../../temp/logs/warn.log`, ...this._optionsLog.file })
+      ]
+    })
     this.logger.warn(data)
     if (process.env.NODE_ENV === 'production') {
-      this._awsS3.uploadObj(this._optionsInfo.file.filename, process.env.BUCKET_LOGS)
+      this._awsS3.uploadObj(`${__dirname}/../../temp/logs/warn.log`, process.env.BUCKET_LOGS)
     }
   }
 }
